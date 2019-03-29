6584de58401f7f21d2dd4ae33dae210a77c75a0a
caldast <caldest1984@gmail.com>
2019-03-29 16:11:05 -0700

    Cleanup

--- Caldast.AlgoLife/Sorting_Searching/SelectionSort.cs
+++ Caldast.AlgoLife/Sorting_Searching/SelectionSort.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Caldast.AlgoLife.Sorting_Searching
+﻿namespace Caldast.AlgoLife.Sorting_Searching
 {
     public class SelectionSort
     {
